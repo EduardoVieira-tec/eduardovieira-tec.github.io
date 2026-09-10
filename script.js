5e0dec9084824e80e4a3796d0e8413268da2c60e
EduardoVieira-tec <eduardovsfilho@gmail.com>
2026-09-10 14:55:56 -0300

    Add contact button alert functionality

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoContato = document.querySelector(".botao-secundario");
+
+botaoContato.addEventListener("click", function () {
+    alert("Obrigado pelo interesse! Veja meu e-mail na seção de contato.");
+});
